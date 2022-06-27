8ce32f43255fc735e8a08ef2a4f404f924c74d38
dadams2001 <dadams22@nd.edu>
2022-06-26 22:01:55 -0500

    added navigation and home.js

---
+++ src/pages/home/home.js
@@ -0,0 +1,23 @@
+import './home.css';
+
+function Home() {
+    return (
+      <div className="Home">
+        <header className='Home-header'>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HOME
+          </a>
+        </header>
+      </div>
+    );
+}
+
+export default Home;
