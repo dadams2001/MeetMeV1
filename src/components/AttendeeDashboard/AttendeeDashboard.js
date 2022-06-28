c5385eb4cf615a0cee991784fdfd11b4a6dbf100
lstark2 <lstark2@nd.edu>
2022-06-28 10:15:43 -0500

    made header cleaner and added attendees

--- src/components/AttendeeDashboard/AttendeeDashboard.js
+++ src/components/AttendeeDashboard/AttendeeDashboard.js
@@ -1,7 +1,10 @@
+import './attendees.css';
+
 //AttendeeDashboard function concatenates and displays the names on the parse back4app list
 export default function AttendeeDashboard({ attendees }) {
   return (
     <div>
+      <h3 class="listname">Attendees</h3>
       <ul>
         {attendees.map((attendee) => (
           <li key={attendee.get("objectId")}>
