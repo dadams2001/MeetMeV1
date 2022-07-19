8b3d9066278b15721e969234c5b488264b163e39
lstark2 <lstark2@nd.edu>
2022-07-19 18:48:29 -0500

    updated-searcg-geocoder

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -1,19 +1,25 @@
- import React, {useState} from "react";
-import Map,{GeolocateControl, MapProvider, Marker} from "react-map-gl";
-import {render} from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import React, { useState, useRef, useCallback } from 'react'
+import Map, { InteractiveMap } from 'react-map-gl'
+import Geocoder from 'react-map-gl-geocoder'
+import {render} from "react-dom";
 
-import 'mapbox-gl/dist/mapbox-gl.css';
 const MAPBOX_TOKEN = 'pk.eyJ1IjoibHN0YXJrMiIsImEiOiJjbDVqdDV4anIwM2NiM2psNjhpd3cyYTI1In0.bjPfymUND6DBvNHIOOMNDg';
-/*STATELESS CHILD COMPONENT */
-const Address = ({ onChange, onClick }) => {
 
-  const initialViewState = {
+/*STATELESS CHILD COMPONENT */
+const Address = ({onChange, onClick }) => {
+  const [viewport, setViewport] = useState({
     latitude: 41.6764,
     longitude: -86.2520,
-    zoom: 11,
-    bearing: 0
-  };
-  
+    zoom: 8,
+  });
+  const geocoderContainerRef = useRef();
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
   const [settings, setSettings] = useState({
     scrollZoom: true,
     boxZoom: true,
@@ -27,35 +33,38 @@ const Address = ({ onChange, onClick }) => {
     maxZoom: 20
   });
 
-
+ 
 
   return (
     <div>
       <br />
-     <h3>Address</h3>
      <p>Enter Party Location below</p>
-      <form>
-        <input text="test" onChange={onChange}  class= "form-control form-control-lg" />
-        <button type="submit" onClick={onClick} class="btn btn-primary btn-block">
-          Submit
-        </button>
-        </form>
-        <br />
-        <div>
-        <MapProvider> 
+
+        <div
+        ref={geocoderContainerRef}
+        style={{ position: "relative", zIndex: 3}}
+      />
+      <InteractiveMap></InteractiveMap>
       <Map
-      initialViewState ={initialViewState}
-      {...settings}
-      style={{width: 600, height: 400}}
-      mapboxAccessToken={MAPBOX_TOKEN} 
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+        ref={mapRef}
+        {...viewport}
+        {...settings}
+        width="600"
+        height="400"
+        onViewportChange={handleViewportChange}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken={MAPBOX_TOKEN}
       >
-        <Marker longitude={ -86.238899} latitude={41.699280} color="red" />
-        <GeolocateControl />
+        <Geocoder
+          mapRef={mapRef}
+          containerRef={geocoderContainerRef}
+          onViewportChange={handleViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
       </Map>
-      </MapProvider>
+      <p>Copy and paste into your address box!</p>
      </div>
-   </div>
   );
 };
 
